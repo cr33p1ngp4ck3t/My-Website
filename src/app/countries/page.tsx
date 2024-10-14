24bbcf963f8b16f236bd08f30eaedea0ce8b8ad2
AnasKhan44 <anaskhan.ak2005@gmail.com>
2024-10-14 23:15:00 +0500

    fixes

--- src/app/countries/page.tsx
+++ src/app/countries/page.tsx
@@ -1,25 +1,33 @@
 import React from "react";
-import '../styles/style.css'
+import '../styles/style.css';
+
 interface Country {
-    name: string;
-    population: number;
-    size: number;
-    capital: string;
-    }
-export let countriesList:Country[] = [{ name:"Pakistan", population: 235800000, size: 770880, capital: "Islamabad"}, 
-        {name:"India", population: 1417000000, size: 3287000, capital: "Delhi"}, 
-        {name:"Afghanistan",population: 41130000, size: 652864, capital: "Kabul" }]
+  name: string;
+  population: number;
+  size: number;
+  capital: string;
+}
+
+export let countriesList: Country[] = [
+  { name: "Pakistan", population: 235800000, size: 770880, capital: "Islamabad" },
+  { name: "India", population: 1417000000, size: 3287000, capital: "Delhi" },
+  { name: "Afghanistan", population: 41130000, size: 652864, capital: "Kabul" }
+];
 
 export default function CountryName() {
-    return (
-      <>
-        <h1>Countries</h1>
-        <p className="parag">Here are a list of Countries, select one to get further information about it</p>
-        <ul>
-          {countriesList.map((country) => (
-            <li key={country.name} className="liststyle"><a href={`countries/${country.name}`}><button className="bw-button-style">{country.name}</button></a></li>
-          ))}
-        </ul>
-      </>
-    );
-  };
+  return (
+    <>
+      <h1>Countries</h1>
+      <p className="parag">Here are a list of Countries, select one to get further information about it</p>
+      <ul>
+        {countriesList.map((country) => (
+          <li key={country.name} className="liststyle">
+            <a href={`countries/${country.name}`}>
+              <button className="bw-button-style">{country.name}</button>
+            </a>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
